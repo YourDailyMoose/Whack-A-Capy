5fd799c9df2d851087e1f4daeca57511acf9c8f1
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2024-08-22 09:49:10 +1000

    Initial Commit

---
+++ capy.js
@@ -0,0 +1,36 @@
+let currentCapyTile;
+
+window.onload = function () {
+    setGame();
+}
+
+function setGame() {
+
+    //set up game board
+    for (let i = 0; i < 9; i++) {
+        //we want 9 divs - 0 to 8
+        let tile = document.createElement("div");
+        tile.id = i.toString();
+        document.getElementById("board").appendChild(tile);
+    }
+    setInterval(setCapy, 1500);
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+
+}
+
+function setCapy() {
+    let capy = document.createElement("img");
+    capy.src = './assets/images/capy.png';
+
+    //get random tile
+    let tile = getRandomTile();
+    currentCapyTile = document.getElementById(tile);
+    currentCapyTile.appendChild(capy);
+    console.log(`Tile ${tile} has been selected.`)
+
+}
+
